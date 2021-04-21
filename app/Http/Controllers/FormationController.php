a703930ca0fb27706bce072619a14705b5b55f23
Abdoul-Aziz ZOROM <abdoulazizzorom@gmail.com>
2021-04-21 14:36:25 +0000

    First commit

---
+++ app/Http/Controllers/FormationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produit;
+
+class FormationController extends Controller
+{
+    public function index()
+    {
+        $produits = Produit::all();
+        $produit = Produit::first();
+
+        $produit2 = Produit::where('prix', '<', 500000)->where('quantite', '!=', 50)->get();
+
+        dd($produit2);
+    }
+
+    public function ajouterProduit()
+    {
+        $produit = new Produit();
+
+        $produit->designation = 'Maxwell';
+        $produit->prix = 8000;
+        $produit->description = "Maxwell c'est un super café !";
+        $produit->quantite = 50;
+        $produit->save();
+        dd($produit);
+    }
+
+    public function ajouterProduit2()
+    {
+        $produit = Produit::create([
+            'designation' => 'Ordinateur',
+            'prix' => 500000,
+            'description' => 'La description de ordinateur',
+            'quantite' => 30,
+        ]);
+
+        dd($produit);
+    }
+
+    public function updateProduit()
+    {
+        $produit1 = Produit::first();
+
+        $produit1->designation = 'Avovita';
+        $produit1->prix = 1800;
+        $produit1->description = "Pommade féminine à base d'avocat !";
+        $produit1->quantite = 10;
+        $produit1->save();
+
+        dd($produit1);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\FormationController;
+use App\Http\Controllers\MainController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [MainController::class, 'accueil']);
+
+Route::get('index', [FormationController::class, 'index']);
+Route::get('ajouter-produit', [FormationController::class, 'ajouterProduit']);
+Route::get('ajouter-produit-2', [FormationController::class, 'ajouterProduit2']);
+
+Route::get('update-produit', [FormationController::class, 'updateProduit']);
